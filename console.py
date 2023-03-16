a0e9f7b69e94a69d542289a72c65d5249b4f22ec
michaelmensa <michaelmensa@live.com>
2023-03-16 15:40:31 +0000

    update on console

--- console.py
+++ console.py
@@ -11,7 +11,7 @@ from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
-form models.review import Review
+from models.review import Review
 
 
 def parse(line):
